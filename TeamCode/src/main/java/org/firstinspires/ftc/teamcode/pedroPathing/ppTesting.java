1d5ccc740a07a72898f35d56b4ef4b5e45328099
Jeff Schwennesen <xjschwen@gmail.com>
2026-02-26 13:17:13 -0500

    fixes as asked for.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ppTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/ppTesting.java
@@ -4,16 +4,14 @@ import com.bylazar.configurables.PanelsConfigurables;
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
+
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
-import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Common.Settings;
 import org.firstinspires.ftc.teamcode.Hardware.Intake;
@@ -28,39 +26,34 @@ public class ppTesting extends OpMode{
     public static Pose scorePose = new Pose(57, 15, Math.toRadians(114)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
     //private final Pose scorePose = new Pose(wallScoreX, wallScoreY, wallScoreH); // seeing if configurables work for this. Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
     public static Pose scorePoseAP =new Pose(52,18,Math.toRadians(10));
+    public static Pose pickup1aPose = new Pose(0,0,0);
+    public static Pose pickup1bPose = new Pose(0,0,0);
 
     private PathChain scorePreload;
     private PathChain grabPickup1, grabPickup1a, grabPickup1b, grabPickup1c, scorePickup1, grabPickup2a,grabPickup2b, scorePickup2 ,goEndPose, goEndPose2, endPath;
     private PathChain cyclePickup1;
 
-
-
-    cyclePickup1 = follower.pathBuilder()
-            .addPath(new BezierLine(scorePose, pickup1aPose))
-            .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1aPose.getHeading())
-
-            .addPath(new BezierLine(pickup1aPose, pickup1bPose))
-            .setLinearHeadingInterpolation(pickup1aPose.getHeading(), pickup1bPose.getHeading())
-            .addPath(new BezierCurve(pickup1bPose, scorePoseAP))
-
-            .setLinearHeadingInterpolation(pickup1bPose.getHeading(), scorePose.getHeading())
-
-            .build();
-
     @Override
     public void init() {
-
-
         follower =  CompBotConstants.createFollower(hardwareMap);
         buildPaths();
         PanelsConfigurables.INSTANCE.refreshClass(this);
         follower.setStartingPose(startPose);
         follower.update();
+    }
 
+    public void buildPaths() {
+        cyclePickup1 = follower.pathBuilder()
+                .addPath(new BezierLine(scorePose, pickup1aPose))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1aPose.getHeading())
 
+                .addPath(new BezierLine(pickup1aPose, pickup1bPose))
+                .setLinearHeadingInterpolation(pickup1aPose.getHeading(), pickup1bPose.getHeading())
+                .addPath(new BezierCurve(pickup1bPose, scorePoseAP))
 
+                .setLinearHeadingInterpolation(pickup1bPose.getHeading(), scorePose.getHeading())
 
-
+                .build();
     }
 
     @Override
